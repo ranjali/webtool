5672fc6d3a81eea6287bffe5ebb9db518311409e
dipta <hazratpradipta.ranjali@thalesgroup.com>
2025-01-10 17:41:40 +0800

    update javascript

--- js/common.js
+++ js/common.js
@@ -1,6 +1,20 @@
+
 function toggleSidebar() {
-    document.getElementsByClassName("sidebar")[0].style.width = 
-    document.getElementsByClassName("sidebar")[0].style.width == "250px" ? "0" : "250px";
-    document.getElementsByClassName("main")[0].style.marginLeft = 
-    document.getElementsByClassName("main")[0].style.marginLeft == "250px" ? "0" : "250px";
+    var sidebar = document.getElementsByClassName("sidebar")[0];
+    var main = document.getElementsByClassName("main")[0];
+    
+    // Handle resize for desktop and mobile
+    if (window.innerWidth > 768) {
+        // Desktop behavior (resize horizontally)
+        sidebar.style.width = 
+        sidebar.style.width == "250px" ? "0" : "250px";
+        main.style.marginLeft = 
+        main.style.marginLeft == "250px" ? "0" : "250px";
+    } else {
+        // Mobile behavior (resize vertically)
+        sidebar.style.width = 
+        sidebar.style.width == "250px" ? "0" : "250px";
+        main.style.marginTop = 
+        main.style.marginTop == "250px" ? "0" : "250px";
+    }
 }
