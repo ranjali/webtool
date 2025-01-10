3072ef1ad722c7b6cd94c41c899021a791e99d1a
dipta <hazratpradipta.ranjali@thalesgroup.com>
2025-01-10 16:48:15 +0800

    fix toggle button

--- js/common.js
+++ js/common.js
@@ -1,7 +1,6 @@
-const menuToggle = document.getElementById('menu-toggle');
-const sidebar = document.getElementById('sidebar');
-
-menuToggle.addEventListener('click', () => {
-    sidebar.classList.toggle('open');
-});
-
+function toggleSidebar() {
+    document.getElementsByClassName("sidebar")[0].style.width = 
+    document.getElementsByClassName("sidebar")[0].style.width == "250px" ? "0" : "250px";
+    document.getElementsByClassName("main")[0].style.marginLeft = 
+    document.getElementsByClassName("main")[0].style.marginLeft == "250px" ? "0" : "250px";
+}
