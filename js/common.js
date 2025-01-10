114faaa55032e9eb8893755cd137eca29c291cf6
dipta <hazratpradipta.ranjali@thalesgroup.com>
2025-01-10 17:51:02 +0800

    revert back since it does not work

--- js/common.js
+++ js/common.js
@@ -1,20 +1,6 @@
-
 function toggleSidebar() {
-    var sidebar = document.getElementsByClassName("sidebar")[0];
-    var main = document.getElementsByClassName("main")[0];
-    
-    // Handle resize for desktop and mobile
-    if (window.innerWidth > 768) {
-        // Desktop behavior (resize horizontally)
-        sidebar.style.width = 
-        sidebar.style.width == "250px" ? "0" : "250px";
-        main.style.marginLeft = 
-        main.style.marginLeft == "250px" ? "0" : "250px";
-    } else {
-        // Mobile behavior (resize vertically)
-        sidebar.style.height = 
-        sidebar.style.height == "100vh" ? "0" : "100vh";
-        main.style.marginTop = 
-        main.style.marginTop == "100vh" ? "0" : "100vh";
-    }
+    document.getElementsByClassName("sidebar")[0].style.width = 
+    document.getElementsByClassName("sidebar")[0].style.width == "250px" ? "0" : "250px";
+    document.getElementsByClassName("main")[0].style.marginLeft = 
+    document.getElementsByClassName("main")[0].style.marginLeft == "250px" ? "0" : "250px";
 }
